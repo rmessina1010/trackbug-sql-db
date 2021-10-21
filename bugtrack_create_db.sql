05cebe708965298680a341d86590f0619b7be76e
Ray Messina <rmessina10@gmail.com>
2021-10-21 02:03:02 -0500

    SQL table set up

---
+++ bugtrack_create_db.sql
@@ -0,0 +1,123 @@
+-- create the database
+DROP DATABASE IF EXISTS bugtrak_db;
+CREATE DATABASE bugtrak_db;
+-- connect via psql
+\c bugtrak_db
+
+-- database configuration
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
+
+---
+--- CREATE tables
+---
+
+CREATE TABLE personel (
+    person_id SERIAL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    reports_to INT,
+    p_role VARCHAR(255),
+	work_stat VARCHAR(255),
+    age   SMALLINT,
+    sex   SMALLINT,
+    PRIMARY KEY(person_id)
+);
+
+
+CREATE TABLE projects (
+    proj_id SERIAL,
+    managed_by INT,
+    proj_title  VARCHAR(255) NOT NULL,
+    proj_status VARCHAR(255) NOT NULL DEFAULT 'Pending/Limbo',
+    proj_excerpt TEXT,
+    CONSTRAINT fk_manager FOREIGN KEY (managed_by) REFERENCES personel (person_id), 
+    PRIMARY KEY(proj_id)
+);
+ 
+
+
+CREATE TABLE techs (
+    tech_id SERIAL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(tech_id)
+);
+
+
+CREATE TABLE skills (
+    skill_id SERIAL,
+    tech INT,
+    skill_name VARCHAR(255) NOT NULL UNIQUE,
+    lev SMALLINT DEFAULT 0 ,
+    PRIMARY KEY(skill_id),
+    CONSTRAINT fk_tech FOREIGN KEY (tech) REFERENCES techs (tech_id) 
+);
+
+
+CREATE TABLE devs_skills (
+    person_id INT,
+    skill_id INT,
+    CONSTRAINT fk_dev FOREIGN KEY (person_id) REFERENCES personel (person_id) ,
+    CONSTRAINT fk_skill FOREIGN KEY (skill_id) REFERENCES skills (skill_id) ,
+    PRIMARY KEY(person_id , skill_id)
+);
+
+
+CREATE TABLE bugs (
+    bug_id SERIAL,
+    bug_title VARCHAR(255) NOT NULL,
+    bug_summary TEXT NOT NULL,
+    bug_status VARCHAR(255),
+    in_proj INT NOT NULL,
+    asigned_to INT,
+    defined_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_project FOREIGN KEY (in_proj) REFERENCES projects (proj_id) ,
+    CONSTRAINT fk_dev FOREIGN KEY (asigned_to) REFERENCES personel (person_id) ,
+    PRIMARY KEY(bug_id)
+);
+
+CREATE TABLE reports (
+    report_id SERIAL,
+    reported_by INT NOT NULL,
+    reported_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    subject VARCHAR(255) NOT NULL,
+    description TEXT,
+    in_project INT NOT NULL,
+    defined_as  INT,
+    CONSTRAINT fk_project FOREIGN KEY (in_project) REFERENCES projects (proj_id) ,
+    CONSTRAINT fk_bug FOREIGN KEY (defined_as) REFERENCES bugs (bug_id) ,
+    CONSTRAINT fk_reporter FOREIGN KEY (reported_by) REFERENCES personel (person_id) ,
+    PRIMARY KEY(report_id)
+);
+
+
+
+CREATE TABLE bug_skills (
+    bug_id INT,
+    skill_id INT,
+    CONSTRAINT fk_bug FOREIGN KEY (bug_id) REFERENCES bugs (bug_id) ,
+    CONSTRAINT fk_skill FOREIGN KEY (skill_id) REFERENCES skills (skill_id) ,
+    PRIMARY KEY(bug_id , skill_id)
+);
+
+
+CREATE TABLE comments (
+    comment_id SERIAL,
+    text TEXT NOT NULL,
+    comm_author INT NOT NULL,
+    refers_to INT NOT NULL,
+    comm_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_author FOREIGN KEY (comm_author) REFERENCES personel (person_id) ,
+    CONSTRAINT fk_refers_to FOREIGN KEY (refers_to ) REFERENCES bugs (bug_id) ,
+    PRIMARY KEY(comment_id)
+);
+
+
+
---
+++ default_data.sql
@@ -0,0 +1,13 @@
+-- default tech
+INSERT INTO techs (name) VALUES ('Javascript'), ('React'), ('HTML'),('CSS'), ('SQL'),('NodeJS'),('Python'), ('PHP');
+
+-- default skills
+INSERT INTO skills (tech, skill_name, lev) VALUES 
+(1,'JS Jr',1), (1,'JS Mid-tier',2),(1,'JS Expert',3), 
+(2,'React Jr',1), (2,'React Mid-tier',2),(2,'React Expert',3), 
+(3,'HTML Jr',1), (3,'HTML Mid-tier',2),(3,'HTML Expert',3), 
+(4,'CSS Jr',1), (4,'CSS Mid-tier',2),(4,'CSS Expert',3), 
+(5,'SQL Jr',1), (5,'SQL Mid-tier',2),(5,'SQL Expert',3), 
+(6,'Node Jr',1), (6,'Node Mid-tier',2),(6,'Node Expert',3), 
+(7,'Python Jr',1), (7,'Python Mid-tier',2),(7,'Python Expert',3), 
+(8,'PHP Jr',1), (8,'PHP Mid-tier',2),(8,'PHP Expert',3);
